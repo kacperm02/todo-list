c490304517f3de54a14e4b3d4244e8faeff822cd
Kacper Mierkiewicz <kmierkiewicz02@gmail.com>
2023-01-29 17:25:24 +0100

    removing tasks and toggle task done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 {
     const tasks = [
         {
+            content: "obejrzyj film",
+            done:true,
+        },
+        {
             content: "zrobić stronę",
             done:false,
         }
@@ -12,19 +16,47 @@
         for(const task of tasks) {
             htmlString += `
                 <li class="list__item${task.done ? " list__item--done" : ""}">
-                    ${task.content}
+                <button class="js-doneTask">Zrobione?</button>
+                <button class="js-removeTask">Usuń</button>
+                ${task.content}
                 </li>
             `;
         }
 
         document.querySelector(".js-tasksList").innerHTML = htmlString;
-    }
+        
+        const removeButtons = document.querySelectorAll(".js-removeTask");
+
+        removeButtons.forEach((removeButtons, index) =>{
+            removeButtons.addEventListener("click", () =>{
+                removeTask(index);
+            })
+        })
+
+        const toggleDoneButtons = document.querySelectorAll(".js-doneTask");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) =>{
+            toggleDoneButton.addEventListener("click", () =>{
+                toggleTaskDone(index);
+            })
+        })
+    };
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
     };
 
